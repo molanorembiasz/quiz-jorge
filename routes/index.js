41df22e285546c21a41a44c57dd53c2a0c9f3853
Molano <molano@wiraby.com>
2015-07-22 14:51:20 +0200

    pagina de creditos con autor incluida

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: 'Express' });
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
